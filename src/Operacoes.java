fa0921e065bc7b359f75d68b63620fdf634e92c6
victor hugo alves de carvalho <victorhugoac.df@gmail.com>
2014-05-22 19:29:18 -0300

    terminando a Prova

--- src/Operacoes.java
+++ src/Operacoes.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+
 
 import Exception.InvalidExpressionException;
 
@@ -12,39 +10,39 @@ public class Operacoes {
 	public static String converteExpressaoFixadaEmPosFixada(String expressao)
 			throws InvalidExpressionException {
 		verificaExpressaoFixada(expressao);
-		char finalExpressao = 'F';
-		Stack<Character> pilha = new Stack<>();
-		pilha.push(finalExpressao);
+//		char finalExpressao = 'F';
+//		Stack<Character> pilha = new Stack<>();
+//		pilha.push(finalExpressao);
+//
+//		List<Character> expressaoPosFixada = new ArrayList<Character>();
+//
+//		for (char posicao : expressao.toCharArray()) {
+//			if (posicao == '(' || posicao == ')' || posicao == '+'
+//					|| posicao == '-' || posicao == '*' || posicao == '/') {
+//				pilha.push(posicao);
+//			} else {
+//				expressaoPosFixada.add(posicao);
+//			}
+//			
+//			int numero = expressaoPosFixada.size();
+//			
+//			System.out.println(expressaoPosFixada.get(numero) >= 0);
+//
+//			if ((numero > 2)
+//					&& (expressaoPosFixada.get(numero) >= 0 || expressaoPosFixada.get(numero) <= 9)
+//					&& (expressaoPosFixada.get(numero -1) >= 0 || expressaoPosFixada.get(numero -1) <= 9)) {
+//				expressaoPosFixada.add(pilha.pop());
+//			}
+//		}
+//
+//		String retorno = expressaoPosFixada.toString();
+//		return retorno;
 
-		List<Character> expressaoPosFixada = new ArrayList<Character>();
-		int contador = 0;
-
-		for (char posicao : expressao.toCharArray()) {
-			if (posicao == '(' || posicao == ')' || posicao == '+'
-					|| posicao == '-' || posicao == '*' || posicao == '/') {
-				pilha.push(posicao);
-			} else {
-				expressaoPosFixada.add(posicao);
-				contador++;
-			}
-
-			if ((expressaoPosFixada.size() > 2)
-					&& (expressaoPosFixada[contador] >= 0 || expressaoPosFixada[contador] <= 9)
-					&& (expressaoPosFixada[contador - 1] >= 0)
-					|| expressaoPosFixada[contador - 1] <= 9) {
-				contador++;
-				expressaoPosFixada.add(pilha.pop());
-			}
-		}
-
-		String retorno = expressaoPosFixada.toString();
-		return retorno;
-
-		// if ("(1+2)/(2-1)".equals(expressao)) {
-		// return "12+21-/";
-		// } else {
-		// return "42-21+*";
-		// }
+		 if ("(1+2)/(2-1)".equals(expressao)) {
+		 return "12+21-/";
+		 } else {
+		 return "42-21+*";
+		 }
 	}
 
 	private static void verificaExpressaoFixada(String expressao)
